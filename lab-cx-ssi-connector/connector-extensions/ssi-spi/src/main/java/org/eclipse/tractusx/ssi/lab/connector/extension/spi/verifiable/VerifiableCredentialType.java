03476be1e16afeddc3879ae6d810b7bfb8d10aeb
Koptan <abdo.koptan@gmail.com>
2023-05-24 20:27:15 +0200

    fix:merge issues

--- lab-cx-ssi-connector/connector-extensions/ssi-spi/src/main/java/org/eclipse/tractusx/ssi/lab/connector/extension/spi/verifiable/VerifiableCredentialType.java
+++ lab-cx-ssi-connector/connector-extensions/ssi-spi/src/main/java/org/eclipse/tractusx/ssi/lab/connector/extension/spi/verifiable/VerifiableCredentialType.java
@@ -1,9 +1,5 @@
-<<<<<<<< HEAD:lab-cx-ssi-connector/connector-extensions/ssi-spi/src/main/java/org/eclipse/tractusx/ssi/lab/connector/extension/spi/verifiable/VerifiableCredentialType.java
-package org.eclipse.tractusx.ssi.lab.connector.extension.spi.verifiable;
-========
-package org.eclipse.tractusx.ssi.lib.model.verifiable.credential;
->>>>>>>> main:cx-ssi-lib/src/main/java/org/eclipse/tractusx/ssi/lib/model/verifiable/credential/VerifiableCredentialType.java
 
+package org.eclipse.tractusx.ssi.lab.connector.extension.spi.verifiable;
 public class VerifiableCredentialType {
   public static final String VERIFIABLE_CREDENTIAL = "VerifiableCredential";
   public static final String MEMBERSHIP_CREDENTIAL = "MembershipCredential";
