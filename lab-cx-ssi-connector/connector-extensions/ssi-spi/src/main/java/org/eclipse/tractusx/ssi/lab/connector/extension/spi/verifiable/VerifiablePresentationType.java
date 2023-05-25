d01a18d807b6bcea08859a67d8d79378fbe004ce
Dominik Pinsel <dominik.pinsel@mercedes-benz.com>
2023-05-25 14:07:32 +0200

    initial commit
    Signed-off-by: Dominik Pinsel <dominik.pinsel@mercedes-benz.com>

--- lab-cx-ssi-connector/connector-extensions/ssi-spi/src/main/java/org/eclipse/tractusx/ssi/lab/connector/extension/spi/verifiable/VerifiablePresentationType.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.tractusx.ssi.lab.connector.extension.spi.verifiable;
-
-public class VerifiablePresentationType {
-  public static final String VERIFIABLE_PRESENTATION = "VerifiablePresentation";
-}
