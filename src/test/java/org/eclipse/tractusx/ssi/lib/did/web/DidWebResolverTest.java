f27b988903322fefed3b5bd44485e1ff2018c786
Pascal Manaras <manaras@xignsys.com>
2024-03-07 14:45:19 +0100

    use jupiter

--- src/test/java/org/eclipse/tractusx/ssi/lib/did/web/DidWebResolverTest.java
+++ src/test/java/org/eclipse/tractusx/ssi/lib/did/web/DidWebResolverTest.java
@@ -21,7 +21,6 @@
 
 package org.eclipse.tractusx.ssi.lib.did.web;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
