b80260f144d5685f3db263b54902bd001f4b3bbe
Mustafa Alsalfiti <mmsalfiti@gmail.com>
2024-03-14 13:31:02 +0100

    fix:apply spotless

--- src/test/java/org/eclipse/tractusx/ssi/lib/model/verifiable/VerifiableTest.java
+++ src/test/java/org/eclipse/tractusx/ssi/lib/model/verifiable/VerifiableTest.java
@@ -1,5 +1,7 @@
 package org.eclipse.tractusx.ssi.lib.model.verifiable;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.net.URI;
 import java.time.Instant;
 import java.util.List;
@@ -10,37 +12,36 @@ import org.eclipse.tractusx.ssi.lib.model.verifiable.credential.VerifiableCreden
 import org.eclipse.tractusx.ssi.lib.model.verifiable.credential.VerifiableCredentialType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class VerifiableTest {
 
-    @Test
-    void testEqual(){
-        final VerifiableCredentialSubject verifiableCredentialSubject =
-                new VerifiableCredentialSubject(Map.of("test", "test"));
+  @Test
+  void testEqual() {
+    final VerifiableCredentialSubject verifiableCredentialSubject =
+        new VerifiableCredentialSubject(Map.of("test", "test"));
 
-        final VerifiableCredentialBuilder verifiableCredentialBuilder =
-                new VerifiableCredentialBuilder();
-        // Using Builder
-        VerifiableCredential one = verifiableCredentialBuilder
-                .id(URI.create("did:test:id"))
-                .type(List.of(VerifiableCredentialType.VERIFIABLE_CREDENTIAL))
-                .issuer(URI.create("did:test:isser"))
-                .expirationDate(Instant.now().plusSeconds(3600))
-                .issuanceDate(Instant.now())
-                .credentialSubject(verifiableCredentialSubject)
-                .build();
+    final VerifiableCredentialBuilder verifiableCredentialBuilder =
+        new VerifiableCredentialBuilder();
+    // Using Builder
+    VerifiableCredential one =
+        verifiableCredentialBuilder
+            .id(URI.create("did:test:id"))
+            .type(List.of(VerifiableCredentialType.VERIFIABLE_CREDENTIAL))
+            .issuer(URI.create("did:test:isser"))
+            .expirationDate(Instant.now().plusSeconds(3600))
+            .issuanceDate(Instant.now())
+            .credentialSubject(verifiableCredentialSubject)
+            .build();
 
-        VerifiableCredential two = verifiableCredentialBuilder
-                .id(URI.create("did:test:id"))
-                .type(List.of(VerifiableCredentialType.VERIFIABLE_CREDENTIAL))
-                .issuer(URI.create("did:test:isser"))
-                .expirationDate(Instant.now().plusSeconds(3600))
-                .issuanceDate(Instant.now())
-                .credentialSubject(verifiableCredentialSubject)
-                .build();
+    VerifiableCredential two =
+        verifiableCredentialBuilder
+            .id(URI.create("did:test:id"))
+            .type(List.of(VerifiableCredentialType.VERIFIABLE_CREDENTIAL))
+            .issuer(URI.create("did:test:isser"))
+            .expirationDate(Instant.now().plusSeconds(3600))
+            .issuanceDate(Instant.now())
+            .credentialSubject(verifiableCredentialSubject)
+            .build();
 
-        assertEquals(one,two);
-    }
-
+    assertEquals(one, two);
+  }
 }
