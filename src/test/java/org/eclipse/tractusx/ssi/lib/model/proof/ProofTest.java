8f8bb34d71b17bc34f8f6f2c1c1966359f4417cf
Nitin Vavdiya <nitin.vavdiya@smartsensesolutions.com>
2024-04-22 12:26:09 +0530

    test: testa added for JWKVerifcationMethodBuilder

--- src/test/java/org/eclipse/tractusx/ssi/lib/model/proof/ProofTest.java
+++ src/test/java/org/eclipse/tractusx/ssi/lib/model/proof/ProofTest.java
@@ -1,6 +1,12 @@
 package org.eclipse.tractusx.ssi.lib.model.proof;
 
+import java.net.URI;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+import org.eclipse.tractusx.ssi.lib.model.proof.jws.JWSProofConfiguration;
 import org.eclipse.tractusx.ssi.lib.model.proof.jws.JWSSignature2020;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -23,5 +29,29 @@ class ProofTest {
         () -> {
           new Proof(map);
         });
+  }
+
+  @Test
+  void testProofConfiguration() {
+
+    Date now = new Date();
+    DateTimeFormatter formatter =
+        DateTimeFormatter.ofPattern(JWSSignature2020.TIME_FORMAT).withZone(ZoneOffset.UTC);
+    Map<String, Object> standardEntries =
+        Map.of(
+            Proof.TYPE,
+            JWSSignature2020.JWS_VERIFICATION_KEY_2020,
+            JWSSignature2020.JWS,
+            "Something",
+            JWSSignature2020.VERIFICATION_METHOD,
+            URI.create("http://example.com#key-1"),
+            JWSSignature2020.CREATED,
+            formatter.format(now.toInstant()),
+            JWSSignature2020.PROOF_PURPOSE,
+            "Proof Purpose");
+
+    HashMap<String, Object> entries = new HashMap<>(standardEntries);
+    Proof proof = new JWSProofConfiguration(entries);
+    Assertions.assertTrue(proof.isConfiguration());
   }
 }
